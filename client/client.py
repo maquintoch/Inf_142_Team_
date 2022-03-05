ad01f9a2afc5d0168ae5687861e0f8e7d75bdfba
maquintoch <>
2022-03-05 10:58:30 +0100

    Changing on client the constant SERVER_ADDR

--- client/client.py
+++ client/client.py
@@ -4,11 +4,11 @@ client script for stablishing a communication to a server
 from socket import socket
 
 # Creating constans: Server Ip address and the port number used
-SERVER_ADDR = "localhost"
-PORT = 5550
+SERVER_ADDR = ("localhost", 5550)
+
 
 sock = socket()
-sock.connect(SERVER_ADDR, PORT)
+sock.connect(SERVER_ADDR)
 word = "Hola"
 sock.send(word.encode())
 new_word = sock.recv(2048).decode()
