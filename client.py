6205dd5042788d1e2fb8509d221e23415be03295
maquintoch <>
2022-03-06 00:11:16 +0100

    Implementing the communication between server.py and client.py. Returning from team_local.py str(available_champs)

--- client.py
+++ client.py
@@ -8,6 +8,7 @@ from rich.table import Table
 
 from champlistloader import load_some_champs
 from core import Champion, Match, Shape, Team
+import json
 
 # Creating constants: Server Ip address and the port number used
 SERVER_ADDR = ("localhost", 5550)
@@ -17,6 +18,8 @@ sock = socket()
 sock.connect(SERVER_ADDR)
 word = "Hola"
 sock.send(word.encode())
-new_word = sock.recv(2048).decode()
-print(f"From Server : {new_word}")
+#new_word = sock.recv(2048).decode()
+ListOfChampions = sock.recv(2048*2).decode()
+#ListOfChampions = json.loads(ListOfChampions)
+print(f"From Server : {ListOfChampions}")
 sock.close()
